347f6582b370f5541b2e188631ffdb1db161d06a
E-bait <vova.semerunin@mail.ru>
2022-06-11 10:12:48 +0300

    Добавьте файлы проекта.

---
+++ PZ_3_1/PZ_3_1.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+#include <iostream>
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int a;
+	printf("Введите число: ");
+	scanf("%d", &a);
+	int first = (a / 100) % 10;
+	int second = (a / 10) % 10;
+	int third = a % 10;
+	bool vozrastaet = (first < second&& second < third);
+	bool ubivaet = (first > second && second > third);
+	printf("%s\n", (vozrastaet) ? "vozrastaet\n" : (ubivaet) ? "ubivaet\n" : "prosto chislo\n");
+	return 0;
+}
+//;_CRT_SECURE_NO_WARNINGS
---
+++ PZ_3_2/PZ_3_2/PZ_3_2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int a;
+    printf("Введите число: ");
+    scanf("%d", &a);
+    a = (a > 0) ? ++a : a -= 2;
+    cout << "Res: "<< a << endl;
+
+    return 0;
+}
