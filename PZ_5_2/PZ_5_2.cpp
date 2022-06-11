948607ea2c36cb0a125582a4bc6604a785ec41b8
E-bait <vova.semerunin@mail.ru>
2022-06-11 10:02:29 +0300

    Добавьте файлы проекта.

---
+++ PZ_5_2/PZ_5_2.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream>
+#include <math.h>
+using namespace std;
+void TrianglePS(double a, double& P, double& S) {
+	P = 3.0 * a;
+	S = a * a * sqrt(3) / 4;
+}
+int main() {
+	system("chcp 1251");
+	srand((int)time(0));
+	double a, p, s;
+
+	for (int i = 0; i < 3; i++) {
+		a = rand() % 20 + 1;
+		TrianglePS(a, p, s);
+		cout << "№ Треугольника " << (i + 1) << endl;
+		cout << "Размер а = " << a << endl;
+		cout << "P= " << p << endl;
+		cout << "S = " << s << endl;
+		cout << endl;
+	}
+	return 0;
+}
